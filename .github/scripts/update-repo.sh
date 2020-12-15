bf95fe779d146980f66b7d588f818ba4b01d0781
Johannes Nicolai <jonico@github.com>
2020-12-15 16:30:55 +0100

    Initial commit

---
+++ .github/scripts/update-repo.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+scene="$1"
+cp assets/"$scene"/* .
+git config --global user.email "dassencio@github.com"
+git config --global user.name "dassencio"
+git add .
+git commit -m "Update README image to '$scene' scene"
+# git push --set-upstream origin snowman
+git push
---
+++ assets/snowfall/README.md
@@ -0,0 +1,8 @@
+# Chapter 2: Winter arrives 🌨
+
+It's snowing! Mona is so happy! :octocat::blush:
+
+![](./figure.svg)
+
+Will the snow be good enough for Mona to build a snowman? Add a label (any label is fine) to the
+issue you created and refresh this page to find out!
